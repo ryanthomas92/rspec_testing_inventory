a993af29aa3dbc378d926615f6dc92c42ae95373
ryanthomas92 <rthoma11@villanova.edu>
2017-01-31 11:20:19 -0800

    items#show page completed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
 
   def show
+    @item = Item.find_by_id(params[:id])
   end
-  
+
 end
--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -2,10 +2,18 @@ require 'rails_helper'
 
 RSpec.describe ItemsController, type: :controller do
   describe "#show" do
-    it "renders the :show view" do
-      item = Item.create({size:'s', color:'blue', status:'unsold'})
+    let(:item) {Item.create({size:'s', color:'blue', status:'unsold'}) }
+
+    before(:each) do
       get :show, id: item.id
+    end
+
+    it "renders the :show view" do
       expect(response).to render_template(:show)
+    end
+
+    it "assigns @item" do
+      expect(assigns(:item)).to eq(item)
     end
   end
 end
